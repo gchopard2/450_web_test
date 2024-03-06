9d7ed60eceffa43ad7173d147b9588839bde3be5
vgepsic <113019726+vgepsic@users.noreply.github.com>
2024-03-06 11:31:16 +0100

    Created model : book Created views : index + book info

--- epsic_library/urls.py
+++ epsic_library/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from library import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', views.index, name='index'),
+    path('<int:pk>',views.book_info, name="info")
 ]
--- library/models.py
+++ library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.author} | {self.title}'
