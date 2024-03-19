bb90fb3f64cb30baeda23f3d06f90d1906975995
vgepsic <113019726+vgepsic@users.noreply.github.com>
2024-03-19 11:04:45 +0100

    updated model tests for using fixtures

--- library/tests/test_models.py
+++ library/tests/test_models.py
@@ -1,17 +1,24 @@
 import pytest
 
-from django.test import Client
+from django.test import Client, TestCase
 from library.models import Book
 
-@pytest.mark.django_db
-def test_book_model_str():
-    # Create a book using the Book model + DB save
-    Book.objects.create(author = "Dominique Roques", title = "Pico Bogue")
-    book = Book.objects.get(id=1)
-    expected_value = "Dominique Roques | Pico Bogue"
-    
-    assert str(book) == expected_value
+class BookModelTest(TestCase):
 
-@pytest.mark.django_db
-def test_book_model_content():
-    pass    # TBC : remove pass command after complete
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(author = "Dominique Roques", title = "Pico Bogue")
+
+    def test_book_model_str(self):
+        book = Book.objects.get(id=1)
+        expected_value = "Dominique Roques | Pico Bogue"
+        
+        assert str(book) == expected_value
+
+    def test_book_model_content(self):
+        book = Book.objects.get(id=1)
+        author_field = book.author
+        title_field = book.title
+        
+        assert author_field == "Dominique Roques"
+        assert title_field == "Pico Bogue"
