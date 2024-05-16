52161b433bf354c42fadf201641c081c6b16c8b7
Gwenael Chopard <pt22paf@eduvaud.ch>
2024-05-16 09:14:34 +0200

    implemented test model content for dvd

--- library/tests/test_model_dvd.py
+++ library/tests/test_model_dvd.py
@@ -3,14 +3,26 @@ import pytest
 from django.test import Client, TestCase
 from library.models import DVD
 
-class BookModelTest(TestCase):
+class DVDModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        DVD.objects.create(title = "Pico Bogue", duration = 120, pegi = 18, description = "Pico Bogue - a great movie")
+        DVD.objects.create(title = "Pico Bogue", duration = 120, pegi = "16+", description = "Pico Bogue - a great movie")
 
-    def test_book_model_str(self):
+    def test_dvd_model_str(self):
         dvd = DVD.objects.get(id=1)
-        expected_value = "Pico Bogue | 120mn | 18 | Pico Bogue - a great movie"
+        expected_value = "Pico Bogue | 120mn | 16+ | Pico Bogue - a great movie"
         
         assert str(dvd) == expected_value
+    def test_dvd_model_content(self):
+        dvd = DVD.objects.get(id=1)
+
+        title_field = dvd.title
+        duration_field = dvd.duration
+        pegi_field = dvd.pegi
+        description_field = dvd.description
+
+        assert title_field == "Pico Bogue"
+        assert duration_field == 120
+        assert pegi_field == "16+"
+        assert description_field == "Pico Bogue - a great movie"
