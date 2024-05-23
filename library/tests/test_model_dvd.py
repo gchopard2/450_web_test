525c721d412e6d0978ae8b3f407e5f432f1349ad
Gwenael Chopard <pt22paf@eduvaud.ch>
2024-05-23 09:27:51 +0200

    updated test model dvd test

--- library/tests/test_model_dvd.py
+++ library/tests/test_model_dvd.py
@@ -7,22 +7,30 @@ class DVDModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        # Création du DVD
         DVD.objects.create(title = "Pico Bogue", duration = 120, pegi = "16+", description = "Pico Bogue - a great movie")
 
     def test_dvd_model_str(self):
+        # Obtenir le DVD de la BD
         dvd = DVD.objects.get(id=1)
+
+        # Valeur attendue
         expected_value = "Pico Bogue | 120mn | 16+ | Pico Bogue - a great movie"
         
+        # Tester
         assert str(dvd) == expected_value
     def test_dvd_model_content(self):
+        # Obtenir le DVD de la BD
         dvd = DVD.objects.get(id=1)
 
-        title_field = dvd.title
-        duration_field = dvd.duration
-        pegi_field = dvd.pegi
-        description_field = dvd.description
+        # Valeurs attendue
+        title_field = "Pico Bogue"
+        duration_field = 120
+        pegi_field = "16+"
+        description_field = "Pico Bogue - a great movie"
 
-        assert title_field == "Pico Bogue"
-        assert duration_field == 120
-        assert pegi_field == "16+"
-        assert description_field == "Pico Bogue - a great movie"
+        # Tester
+        assert title_field == dvd.title
+        assert duration_field == dvd.duration
+        assert pegi_field == dvd.pegi
+        assert description_field == dvd.description
